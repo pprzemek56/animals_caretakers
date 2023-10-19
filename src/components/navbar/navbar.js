5e782dcb27f7d0ec309b41c54060fd8d6af5f4fe
93k00c3 <pqc997@gmail.com>
2023-10-19 15:31:24 +0200

    Slight navbar changes

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -7,7 +7,7 @@ function Navbar() {
       <ul>
         <li><a href="/">Home</a></li>
         <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
+        <li><a href="/profile" class="button">Profile</a></li>
       </ul>
     </nav>
   );
