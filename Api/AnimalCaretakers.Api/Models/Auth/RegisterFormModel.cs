187a2f3c03e7021ce62bc70ae8a738d23889482b
Michał Woźniak <m.wozniak@ideo.pl>
2023-12-11 22:02:07 +0100

    Validation abstraction

--- Api/AnimalCaretakers.Api/Models/Auth/RegisterFormModel.cs
+++ Api/AnimalCaretakers.Api/Models/Auth/RegisterFormModel.cs
@@ -1,12 +1,29 @@
-﻿using AnimalCaretakers.Data.Enums;
+﻿using AnimalCaretakers.Api.Validation;
+using AnimalCaretakers.Data.Enums;
+using FluentValidation;
+using System.ComponentModel.DataAnnotations;
 
 namespace AnimalCaretakers.Api.Models.Auth;
 
-public class RegisterFormModel
+public class RegisterFormModel : IValidatableObject
 {
     public string GivenName { get; set; }
     public string Surname { get; set; }
     public string Username { get; set; }
     public string Password { get; set; }
     public UserType UserType { get; set; }
+
+    public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+    {
+        return this.Rules(p =>
+        {
+            p.RuleFor(q => q.GivenName).NotEmpty().WithMessage("Pole jest wymagane");
+            p.RuleFor(q => q.Surname).NotEmpty().WithMessage("Pole jest wymagane");
+            p.RuleFor(q => q.Username).NotEmpty().WithMessage("Pole jest wymagane").MinimumLength(3).WithMessage("Nazwa użytkownika musi mieć minimum 3 znaki");
+            p.RuleFor(q => q.Password).NotEmpty().WithMessage("Pole jest wymagane").MinimumLength(5).WithMessage("Hasło musi mieć minimum 5 znaków");
+            p.RuleFor(q => q.UserType).NotEmpty().WithMessage("Pole jest wymagane");
+        })
+        .Validate(this)
+        .Result();
+    }
 }
---
+++ Api/AnimalCaretakers.Api/Validation/ValidationExtensions.cs
@@ -0,0 +1,55 @@
+﻿using FluentValidation;
+
+namespace AnimalCaretakers.Api.Validation;
+
+public interface IFluentValidator<TModel> : IValidator<TModel>
+{
+    IFluentValidator<TModel> Rules(Action<FluentValidator<TModel>> action, bool condition = true);
+}
+
+public static class Validation
+{
+    public static IFluentValidator<TModel> Rules<TModel>(Action<FluentValidator<TModel>> action)
+    {
+        return new FluentValidator<TModel>(action);
+    }
+}
+
+public class FluentValidator<TModel> : AbstractValidator<TModel>, IFluentValidator<TModel>
+{
+    #region FluentValidator()
+    public FluentValidator(Action<FluentValidator<TModel>> action)
+    {
+        action(this);
+    }
+    #endregion
+
+    #region Rules()
+    public IFluentValidator<TModel> Rules(Action<FluentValidator<TModel>> action, bool condition = true)
+    {
+        if (condition)
+        {
+            Include(new FluentValidator<TModel>(action));
+        }
+
+        return this;
+    }
+    #endregion
+}
+
+public static class _ValidationExtensions
+{
+    #region Result()
+    public static IEnumerable<ValidationResult> Result(this FluentValidation.Results.ValidationResult result)
+    {
+        return result.Errors.Select(p => new ValidationResult(p.ErrorMessage, new[] { p.PropertyName }));
+    }
+    #endregion
+
+    #region Rules()
+    public static IFluentValidator<TModel> Rules<TModel>(this TModel model, Action<FluentValidator<TModel>> action)
+    {
+        return Validation.Rules(action);
+    }
+    #endregion
+}
